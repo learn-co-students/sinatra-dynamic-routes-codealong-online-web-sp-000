12b5faede0c6e2d9809c4c9404ae7e8675ab2b4f
Michael Carroll <carrollm2@gmail.com>
2020-02-23 20:52:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -19,12 +19,6 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  # get "/multiply/:num1/:num2" do
-  #   @number1 = params[:num1]
-  #   @number2 = params[:num2]
-  #   "#{24}"
-  # end
-
   get "/multiply/:num1/:num2" do
     @number1 = params[:num1]
     @number2 = params[:num2]
