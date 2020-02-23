938bc78e1a592000af35645a4fa0338c2fd9cae7
ryan m <contact@ryanmeek.com>
2020-02-23 19:42:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ class App < Sinatra::Base
   get('/hello') {"Hello World!"}
   get("/hello/:name") {"Hello #{params[:name]}!"}
   get("/goodbye/:name") {"Goodbye, #{params[:name]}."}
-  get("/multiply/:num1/:num2") {params[:num1].to_i * params[:num2].to_i}
+  get("/multiply/:num1/:num2") {"#{params[:num1].to_i * params[:num2].to_i}"}
 end
