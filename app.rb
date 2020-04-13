df27ba3fb3e385a0c5e10ff75b35c2fa9e7d586b
nathan-stamand <60270171+nathan-stamand@users.noreply.github.com>
2020-04-13 10:16:29 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,22 +7,6 @@ class App < Sinatra::Base
     "Hello World!"
   end
 
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
-
-  # Code your final two routes here:
-  require_relative 'config/environment'
-
-class App < Sinatra::Base
-
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
-  end
-
 
   # This is a sample dynamic route.
   get "/hello/:name" do
@@ -45,4 +29,3 @@ class App < Sinatra::Base
 
 end
 
-end
