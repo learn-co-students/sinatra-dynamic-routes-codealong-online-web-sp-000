5e674d59be5074d6ab71c6bf124d30a7710bb96f
chienleow <chienleow1@gmail.com>
2020-05-30 16:59:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-  @num1 = params[:num1]
-  "#{@num1}"
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    (@num1 * @num2).to_s
   end
 end
