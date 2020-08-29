5fdf94ff45040088c11e274f41e41394295b8cea
milos381 <milos_popovic@outlook.com>
2020-08-29 19:15:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
-    hello = @num1 * num2
-    hello
+    hello = @num1 * @num2
+    "#{hello}"
   end
 
 end
